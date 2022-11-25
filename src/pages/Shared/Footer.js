e6ca9d70b674b587313777d5e36075f3f34727f7
Nahid hassan <nahidlikhon22@gmail.com>
2022-11-25 12:46:58 +0600

    navbar added

--- src/pages/Shared/Footer.js
+++ src/pages/Shared/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div>
-            <h1>this is footer</h1>
+            
         </div>
     );
 };
--- src/pages/Shared/Navabr.js
+++ src/pages/Shared/Navabr.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navabr = () => {
+    const menuItems = <React.Fragment>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/appointment">Appointment</Link></li>
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/reviews">Reviews</Link></li>
+    <li><Link to="/login">Login</Link></li>
+</React.Fragment>
+    
     return (
-        <div>
-           
+        <div className="flex justify-between navbar bg-base-100">
+        <div className="navbar-start">
+            <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                <ul tabIndex={0} className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                    {menuItems}
+                </ul>
+            </div>
+            <Link to="/" className="text-xl normal-case btn btn-ghost">KenaKata.com</Link>
         </div>
+        <div className="hidden navbar-center lg:flex">
+            <ul className="p-0 menu menu-horizontal">
+                {menuItems}
+            </ul>
+        </div>
+    </div>
     );
 };
 
